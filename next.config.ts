9001c9a3fe418db82b1db662f2aa53c4237c747d
mesfin-alamirew <abzmsf@gmail.comgit config --global user.email abzmsf@gmail.comgit config --global user.email abzmsf@gmail.comgit config --listclear>
2025-10-07 11:19:36 +0300

    git deployment workflow

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+  distDir: 'build',
+  output: 'standalone',
 };
 
 export default nextConfig;
