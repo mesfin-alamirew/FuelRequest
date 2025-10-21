6d0f0cc10fb70b2f830cf406152a0790e478cf4d
mesfin-alamirew <abzmsf@gmail.comgit config --global user.email abzmsf@gmail.comgit config --global user.email abzmsf@gmail.comgit config --listclear>
2025-10-21 09:44:55 +0300

    UI Updated to handle theme change

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -20,10 +20,6 @@ export default async function DashboardPage() {
           <h3 className="mb-4 font-semibold text-gray-800 text-theme-xl dark:text-white/90 sm:text-2xl">
             Place for different charts
           </h3>
-          <p className="text-sm text-gray-500 dark:text-gray-400 sm:text-base">
-            Start putting content on grids or panels, you can also use different
-            combinations of grids.Please check out the dashboard and other pages
-          </p>
         </div>
       </div>
     </div>
